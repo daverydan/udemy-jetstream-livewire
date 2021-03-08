0dce3ba8d8db4aa356abd1214f36ebe88ce59730
Danny Avery <danny.w.avery@gmail.com>
2021-03-08 16:39:35 -0500

    Items: table markup + pagination

--- resources/views/livewire/items.blade.php
+++ resources/views/livewire/items.blade.php
@@ -1,3 +1,36 @@
-<div>
-    Livewire Items
+<div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+    <div class="mt-8 text-2x1">
+        Items
+    </div>
+
+    @php
+        $theads = ['ID', 'Name', 'Price', 'Status', 'Action'];
+    @endphp
+
+    <div class="mt-6">
+        <table class="table-auto w-full">
+            <thead>
+                @foreach ($theads as $th)
+                    <th class="px-4 py-2">
+                        <div class="flex-items-center">{{ $th }}</div>
+                    </th>
+                @endforeach
+            </thead>
+            <tbody>
+                @foreach ($items as $item)
+                    <tr>
+                        <th class="border px-4 py-2">{{ $item->id }}</th>
+                        <th class="border px-4 py-2">{{ $item->name }}</th>
+                        <th class="border px-4 py-2">{{ number_format($item->price, 2) }}</th>
+                        <th class="border px-4 py-2">{{ $item->status ? 'Active' : 'Not Active' }}</th>
+                        <th class="border px-4 py-2">Edit | Delete</th>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+    <div class="mt-4">
+        {{ $items->links() }}
+    </div>
 </div>
