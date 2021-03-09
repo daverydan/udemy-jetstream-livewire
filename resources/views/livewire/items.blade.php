6b52c8ec79593f1c08d95b344bbf4e5867785dd4
Danny Avery <danny.w.avery@gmail.com>
2021-03-08 21:30:49 -0500

    Edit + Flash messages

--- resources/views/livewire/items.blade.php
+++ resources/views/livewire/items.blade.php
@@ -1,4 +1,30 @@
 <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+    @if (session('message'))
+        <div class="rounded-md bg-blue-50 p-4" x-data="{show: true}" x-show="show">
+            <div class="flex">
+                <div class="flex-shrink-0">
+                    <svg class="h-5 w-5 text-blue-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd" />
+                    </svg>
+                </div>
+                <div class="ml-3">
+                    <h3 class="text-sm font-medium text-blue-800">{{ session('message') }}</h3>
+                </div>
+                <div class="ml-auto pl-3">
+                    <div class="-mx-1.5 -my-1.5">
+                        <button class="inline-flex bg-blue-50 rounded-md p-1.5 text-blue-500 hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-blue-50 focus:ring-blue-600" @click="show = false">
+                            <span class="sr-only">Dismiss</span>
+                            <!-- Heroicon name: solid/x -->
+                            <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
+
     <h1 class="mt-8 text-xl font-bold flex justify-between">
         <div>Items</div>
         <div class="mr-2">
@@ -61,7 +87,9 @@
                             <th class="border px-4 py-2">{{ $item->status ? 'Active' : 'Not Active' }}</th>
                         @endif
                         <th class="border px-4 py-2">
-                            Edit
+                            <x-jet-button wire:click="confirmItemEdit({{ $item->id }})" class="bg-blue-500 hover:bg-blue-700">
+                                Edit
+                            </x-jet-button>
                             <x-jet-danger-button wire:click="confirmItemDeletion({{ $item->id }})" wire:loading.attr="disabled">
                                 Delete
                             </x-jet-danger-button>
@@ -77,7 +105,7 @@
     </div>
 
     {{-- Modals --}}
-    <x-jet-dialog-modal wire:model="confirmingItemDeletion">
+    <x-jet-confirmation-modal wire:model="confirmingItemDeletion">
         <x-slot name="title">
             {{ __('Delete Item') }}
         </x-slot>
@@ -95,11 +123,11 @@
                 {{ __('Delete') }}
             </x-jet-danger-button>
         </x-slot>
-    </x-jet-dialog-modal>
+    </x-jet-confirmation-modal>
 
     <x-jet-dialog-modal wire:model="confirmingItemAdd">
         <x-slot name="title">
-            {{ __('Add Item') }}
+            {{ isset($this->item->id) ? 'Edit Item' : 'Add Item' }}
         </x-slot>
 
         <x-slot name="content">
